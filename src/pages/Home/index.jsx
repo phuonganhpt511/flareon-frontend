0f31c4e67f76cc4cc94edb11bf46707499585bb2
Pham Phuong Anh <phamphuonganh5112002@gmail.com>
2025-09-29 00:15:53 +0700

    feat: :sparkles: install lib and config project base

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quae corporis fuga aperiam consectetur aspernatur
+        harum? Id, quibusdam magnam ut ullam illum sit alias eligendi exercitationem voluptatem aspernatur harum velit!
+      </p>
+    </>
+  );
+};
+
+export default Home;
