e116e63c1950c41f02fa8dd679b20958c99970ba
mducw <amduc374@gmail.com>
2025-10-23 13:35:56 +0700

    chore: validate-quan-ly-danh-muc

--- src/pages/admin/CategoryManagement/CategoryFormModal.jsx
+++ src/pages/admin/CategoryManagement/CategoryFormModal.jsx
@@ -2,6 +2,16 @@ import { Modal, Form, Input, Select } from 'antd'
 import { DEFAULT_FORM_VALUES } from '@/shared/constants/category'
 
 const CategoryFormModal = ({ open, title, submitting, form, onOk, onCancel }) => {
+
+  const validateUrl = (_, value) => {
+    if (!value) return Promise.resolve()
+    try {
+      new URL(value)
+      return Promise.resolve()
+    } catch {
+      return Promise.reject(new Error('Vui lòng nhập URL hợp lệ'))
+    }
+  }
   return (
     <Modal
       title={title}
@@ -15,19 +25,29 @@ const CategoryFormModal = ({ open, title, submitting, form, onOk, onCancel }) =>
     >
       <Form form={form} layout="vertical" initialValues={DEFAULT_FORM_VALUES}>
         <Form.Item
-          label="Tên danh mục"
           name="category_name"
-          rules={[{ required: true, message: 'Vui lòng nhập tên danh mục' }]}
+          label="Tên danh mục"
+          rules={[
+            { required: true, message: 'Vui lòng nhập tên danh mục' },
+            { min: 2, message: 'Tên ít nhất 2 ký tự' },
+            { max: 100, message: 'Tên tối đa 100 ký tự' },
+          ]}
         >
-          <Input placeholder="Nhập tên danh mục" allowClear/>
+          <Input placeholder="Nhập tên danh mục" allowClear />
         </Form.Item>
 
-        <Form.Item label="Mô tả" name="description">
-          <Input.TextArea placeholder="Mô tả..." rows={3} allowClear/>
+        <Form.Item name="description" label="Mô tả" rules={[{ max: 500, message: 'Mô tả tối đa 500 ký tự' }]}>
+          <Input.TextArea rows={4} placeholder="Mô tả (tùy chọn)" allowClear />
         </Form.Item>
 
-        <Form.Item label="Ảnh" name="imageUrl">
-          <Input placeholder="URL ảnh" allowClear/>
+        <Form.Item
+          name="imageUrl"
+          label="URL ảnh"
+          rules={[
+            { required: true, message: 'Vui lòng nhập URL ảnh' },
+            { validator: validateUrl }
+          ]}>
+          <Input placeholder="http://..." allowClear />
         </Form.Item>
 
         <Form.Item
